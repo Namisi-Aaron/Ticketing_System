99ca313a9d4055a5c91f1ec2992398ae4a3efb9d
Namisi-Aaron <aaronnamisi@gmail.com>
2023-10-03 16:54:38 +0300

    login page

---
+++ ticketingSys/client/src/components/login.vue
@@ -0,0 +1,45 @@
+<!-- Login.vue -->
+<template>
+    <div>
+        <hi>Login</hi>
+        <br>
+        <input
+          type="username"
+          name="username"
+          v-model="username"
+          placeholder="Username"/>
+          <br>
+        <input
+          type="password"
+          name="password"
+          v-model="password"
+          placeholder="Password"/>
+        <br>
+        <button @click="login">Login</button>
+    </div>
+  </template>
+  
+  <script>
+  import authenticationService from '../services/authentication'
+
+  export default {
+    data() {
+      return {
+        username: '',
+        password: '',
+      };
+    },
+    methods: {
+      async login () {
+      const response = await authenticationService.login({
+        username: this.username,
+        password: this.password
+      })
+        console.log(response.data)
+      }
+    }
+  };
+  </script>
+  <style scoped>
+
+</style>
